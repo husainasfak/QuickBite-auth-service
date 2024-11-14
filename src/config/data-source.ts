74104707834c1acccaf37bdb253866577ec3a14b
Ashfaq Hussain <asfak@vcbay.co>
2024-11-14 06:10:11 +0530

    tenant table

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,7 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
-import { User } from '../entity/User'
+
 import { Config } from '.'
-import { RefreshToken } from '../entity/RefreshToken'
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
@@ -14,7 +13,7 @@ export const AppDataSource = new DataSource({
     // do not use this in production, keep always false
     synchronize: false,
     logging: false,
-    entities: [User, RefreshToken],
+    entities: ['src/entity/*.ts'],
     migrations: ['src/migration/*.ts'],
     subscribers: [],
 })
