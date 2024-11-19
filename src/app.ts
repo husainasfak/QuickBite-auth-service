b4615913b11949b1e3e8825a866c38df1b89858c
Ashfaq Hussain <asfak@vcbay.co>
2024-11-19 06:48:46 +0530

    add cors policy

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,14 @@ import authRouter from './routes/auth'
 import tenantRoutes from './routes/tenant'
 import userRoutes from './routes/user'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app = express()
+app.use(
+    cors({
+        origin: ['http://localhost:5173'],
+        credentials: true,
+    }),
+)
 
 app.use(express.static('public'))
 
