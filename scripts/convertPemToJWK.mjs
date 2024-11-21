7278bfa91f582ad9bd2070ae895ba79acc35ce4d
Ashfaq Hussain <asfak@vcbay.co>
2024-11-21 06:39:23 +0530

    format

--- scripts/convertPemToJWK.mjs
+++ scripts/convertPemToJWK.mjs
@@ -1,9 +1,9 @@
-import fs from "fs";
-import rsaPemToJwk from "rsa-pem-to-jwk";
+import fs from 'fs'
+import rsaPemToJwk from 'rsa-pem-to-jwk'
 
-const privateKey = fs.readFileSync("./certs/private.pem");
+const privateKey = fs.readFileSync('./certs/private.pem')
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-rsaPemToJwk(privateKey, { use: "sig" }, "public");
+rsaPemToJwk(privateKey, { use: 'sig' }, 'public')
 
 // console.log(JSON.stringify(jwk));
